30f930911c5496dbc8cc826b4edaad919e90a4b5
yjjuneyjj <yjjuneyjj@hotmail.com>
2020-12-16 17:23:20 +0800

    20201216-1723

--- jd_jdh.js
+++ jd_jdh.js
@@ -6,7 +6,7 @@
 ============Quantumultx===============
 [task_local]
 #京东健康
-10 8 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_jdh.js, tag=京东健康, enabled=true
+10 8 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_jdh.js, tag=京东健康, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_jdh.png, enabled=true
 
 ================Loon==============
 [Script]
@@ -106,7 +106,7 @@ async function jdJdh() {
   await queryTask(18,"2951198") // 看名医直播
   await queryTask(17,"246147") //
   await queryTask(24, t) // 辟谣
-  // await queryTask(22,"20201215") // TODO：打卡尚未完成
+  await doTask(22,42,`${new Date().getUTCFullYear()}-${new Date().getUTCMonth()+1}-${new Date().getUTCDate()}`) // 去打卡
   await queryTask(20,"362451650500001") // 测一测
   await doTask(23,40,`${rand(10000, 20000)}`) // 走路，这个可以直接提示领奖结果
   // 以下两个需要开启家庭医生才能完成
